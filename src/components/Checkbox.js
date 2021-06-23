f18ad5f3432cf7c0bacf290c5756fe2344665bb4
Irina Turenko <turenko7irina@mail.ru>
2021-06-23 18:45:30 +0300

    upd

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -2,12 +2,21 @@ import React from 'react'
 
 const Checkbox = () => {
   return (
-    <form className='stops-value'>
-    <label>Все<input type="checkbox"/><span class="checkmark"></span></label>
-    <label>1 пересадка<input type="checkbox"/><span class="checkmark"></span></label>
-    <label>2 пересадки<input type="checkbox"/><span class="checkmark"></span></label>
-    <label>3 пересадки<input type="checkbox"/><span class="checkmark"></span></label>
-    </form>
+    <div className='stops-value'>
+    <h3 className='page-header'>Количество пересадок</h3>
+      <div>
+        <input type='checkbox' id='cb1' /> <label for='cb1'> Все</label>
+      </div>
+      <div>
+        <input type='checkbox' id='cb2' /> <label for='cb2'>1 пересадка</label>
+      </div>
+      <div>
+        <input type='checkbox' id='cb3' /> <label for='cb3'>2 пересадки</label>
+      </div>
+      <div>
+        <input type='checkbox' id='cb4' /> <label for='cb4'>3 пересадки</label>
+      </div>
+    </div>
   )
 }
 
